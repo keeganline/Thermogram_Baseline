e1b86eec1fae9e078a2f91935bd69beb98bf5392
keeganline <ktl56@nau.edu>
2024-03-05 17:55:54 -0700

    remove outer layer
    outer layer

---
+++ R/UrineWorking.R
@@ -0,0 +1,11 @@
+#' Clean Urine Thermogram Data
+#'
+#' A data frame containing 393 urine thermograms in "long" format. Each sample has two iterations "a" and "b" for a total of 781 thermograms.
+#'
+#' @format A data frame with 3118189 observations with 3 columns.
+#' \describe{
+#'    \item{SampleID}{A unique identifier for each sample and iteration}
+#'    \item{Temperature}{The temperature measured in degrees celsius}
+#'    \item{dCp}{The dCp at the given temperature}
+#' }
+"UrineWorking"
