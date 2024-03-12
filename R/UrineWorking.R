9e52533c06be6e5b5198ec81809d7e8c771eb31c
keeganline <ktl56@nau.edu>
2024-03-12 10:06:47 -0700

    update
    update

--- R/UrineWorking.R
+++ R/UrineWorking.R
@@ -1,8 +1,8 @@
 #' Clean Urine Thermogram Data
 #'
-#' A data frame containing 393 urine thermograms in "long" format. Each sample has two iterations "a" and "b" for a total of 781 thermograms.
+#' A data frame containing 5 urine thermograms in "long" format. Each sample has two iterations "a" and "b" for a total of 10 thermograms.
 #'
-#' @format A data frame with 3118189 observations with 3 columns.
+#' @format A data frame with 5390 observations with 3 columns.
 #' \describe{
 #'    \item{SampleID}{A unique identifier for each sample and iteration}
 #'    \item{Temperature}{The temperature measured in degrees celsius}
